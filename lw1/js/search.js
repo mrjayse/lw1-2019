e4a1789f95c62f8bedb3c934a2024b7831cd9e76
mrjayse <43705694+mrjayse@users.noreply.github.com>
2020-04-07 18:08:42 +0300

    Add files via upload

--- lw1/js/search.js
+++ lw1/js/search.js
@@ -1,10 +1,35 @@
-function Search() {
-    var input = document.getElementById("search-field");
-    var filter = input.value.toLowerCase();
-    var nodes = document.getElementsByClassName("column is-one-third");
+let resultSearchRow;
 
-    for (var i = 0; i < nodes.length; i++) {
-        if (!nodes[i].innerText.toLowerCase().includes(filter))
-            nodes[i].style.display = "none";
+onload = function () {
+  resultSearchRow = document.createElement("div");
+  resultSearchRow.classList.add("row");
+  resultSearchRow.classList.add("columns");
+  resultSearchRow.classList.add("is-multiline");
+  resultSearchRow.classList.add("search-result");
+  document.getElementsByClassName("container")[0].getElementsByClassName("section")[0].appendChild(resultSearchRow);
+}
+
+function Search() {
+  let input = document.getElementById("search-field");
+  if (input.value.replace(/\s/g, "").length) {
+    let filter = input.value.toLowerCase();
+    let rows = document.getElementsByClassName("row");
+    let nodes = document.getElementsByClassName("column is-one-third");
+
+    while (resultSearchRow.firstChild) {
+      resultSearchRow.removeChild(resultSearchRow.firstChild);
     }
+
+    for (let i = 0; i < rows.length; i++) {
+      rows[i].style.display = "none";
+    }
+
+    let nLength = nodes.length;
+    for (let i = 0; i < nLength; i++) {
+      if (nodes[i].innerText.toLowerCase().includes(filter)) {
+        resultSearchRow.appendChild(nodes[i].cloneNode(true));
+      }
+    }
+    resultSearchRow.style.display = "flex";
+  }
 }
