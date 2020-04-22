f5d87254587120bfad8f23a61bb1828ded8e37af
mrjayse <43705694+mrjayse@users.noreply.github.com>
2020-04-22 22:02:26 +0300

    Add files via upload

--- lw1/js/search.js
+++ lw1/js/search.js
@@ -1,27 +1,27 @@
 let resultSearchRow;
 
 onload = function () {
-  resultSearchRow = document.createElement("div");
-  resultSearchRow.classList.add("row");
-  resultSearchRow.classList.add("columns");
-  resultSearchRow.classList.add("is-multiline");
-  resultSearchRow.classList.add("search-result");
-  document.getElementsByClassName("container")[0].getElementsByClassName("section")[0].appendChild(resultSearchRow);
+  resultSearchRow = document.createElement('div');
+  resultSearchRow.classList.add('row');
+  resultSearchRow.classList.add('columns');
+  resultSearchRow.classList.add('is-multiline');
+  resultSearchRow.classList.add('search-result');
+  document.getElementsByClassName('container')[0].getElementsByClassName('section')[0].appendChild(resultSearchRow);
 }
 
 function Search() {
-  let input = document.getElementById("search-field");
-  if (input.value.replace(/\s/g, "").length) {
+  let input = document.getElementById('search-field');
+  if (input.value.replace(/\s/g, '').length) {
     let filter = input.value.toLowerCase();
-    let rows = document.getElementsByClassName("row");
-    let nodes = document.getElementsByClassName("column is-one-third");
+    let rows = document.getElementsByClassName('row');
+    let nodes = document.getElementsByClassName('column is-one-third');
 
     while (resultSearchRow.firstChild) {
       resultSearchRow.removeChild(resultSearchRow.firstChild);
     }
 
     for (let i = 0; i < rows.length; i++) {
-      rows[i].style.display = "none";
+      rows[i].style.display = 'none';
     }
 
     let nLength = nodes.length;
@@ -30,6 +30,6 @@ function Search() {
         resultSearchRow.appendChild(nodes[i].cloneNode(true));
       }
     }
-    resultSearchRow.style.display = "flex";
+    resultSearchRow.style.display = 'flex';
   }
 }
